05a6c620a4351b1097e93de36702389ae67f308a
akunamatata701 <akunamatata701@gmail.com>
2018-07-23 21:37:42 +0300

    Refactored code

--- matrix.js
+++ matrix.js
@@ -1,20 +1,5 @@
 function Matrix() {
-  function rotate(matrix) {
-    if (matrix.length === 0) return [];
-
-    const n = matrix[0].length;
-    const newMatrix = [];
-
-    for (let i = 0; i < n; i += 1) {
-      const row = [];
-      for (let j = n - 1; j >= 0; j -= 1) {
-        row.push(matrix[j][i]);
-      }
-      newMatrix.push(row);
-    }
-
-    return newMatrix;
-  }
+  const rotate = matrix => matrix.reverse().map((row, i) => row.map((item, j) => matrix[j][i]));
 
   return {
     rotate,
@@ -23,12 +8,12 @@ function Matrix() {
 
 module.exports = Matrix();
 
-// const array = [
-//   [1, 2, 3],
-//   [4, 5, 6],
-//   [7, 8, 9],
-// ];
-//
-// /* eslint no-console: "off" */
-// console.log(array);
-// console.log(Matrix().rotate(array));
+const array = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+
+/* eslint no-console: "off" */
+console.log(array);
+console.log(Matrix().rotate(array));
